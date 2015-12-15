8f2eb8951757af65484c1e3e4df52ebdecdd7914
olafurt15 <olafurt15@ru.is>
2015-12-15 16:25:50 +0000

    connectionsmenu.h commentað

--- Verkefni_vika3/connectionsmenu.h
+++ Verkefni_vika3/connectionsmenu.h
@@ -24,10 +24,15 @@ class ConnectionsMenu : public QWidget
 public:
     explicit ConnectionsMenu(QWidget *parent = 0);
     ~ConnectionsMenu();
+    //birtir allar upplýsingar um tengingar í töflunni vinstra megin
     void displayConnectionsVectorInTable(vector <Connections> connectionsVector);
+    //birtir alla scientists í listanum vinstra megin
     void displayPersonVector(vector <person> perVec);
+    //birtir allar tölvur í listanum hægra megin
     void displayComputerVector(vector <Computer> compVec);
+    //finnur valda persónu í vectornum og skilar staðsetningu á persónunni í þeim vector
     int findPersonInVector(vector<person> personVector, string nameOfSelected);
+    //sama og með persónur
     int findComputerInVector(vector <Computer> computerVector, string nameOfSelected);
     void refresh();
 
